7badf20388b2c27a3d709a373dfc11ca96d6de4e
samsoncs <samsoncs@stud.ntnu.no>
2015-04-13 13:29:45 -0700

    added tasks and client

---
+++ src/tasks/TaskEuclideanTsp.java
@@ -0,0 +1,137 @@
+package tasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import api.Task;
+
+/**
+ * This is an implementation of the task interface that solves the traveling salesman problem
+ * @author Samson Svendsen, Simen Aakhus
+ *
+ */
+public class TaskEuclideanTsp implements Task<List<Integer>> {
+
+	double[][] cities;
+	
+	/**
+	 * Constructor that initiates cities
+	 * @param cities A 2D double array of cities, where cities[i][0] gives x-coordinate, and cities[i][1] is the y-coordinate.
+	 */
+	public TaskEuclideanTsp(double[][] cities) {
+		this.cities = cities;
+	}
+
+	/**
+	 * Calculates an adjacency matrix
+	 * @param cities A 2D double array of cities, where cities[i][0] gives x-coordinate, and cities[i][1] is the y-coordinate.
+	 * @return returns a 2D adjacency matrix
+	 */
+	public double[][] calcAdjacencyMatrix(double[][] cities) {
+
+		int numberOfCities = cities.length;
+		double[][] matrix = new double[numberOfCities][numberOfCities];
+
+		for (int i = 0; i < numberOfCities; i++) {
+			double x1 = cities[i][0];
+			double y1 = cities[i][1];
+
+			for (int j = 0; j < numberOfCities; j++) {
+				double x2 = cities[j][0];
+				double y2 = cities[j][1];
+
+				double distance = Math.sqrt(((x2 - x1) * (x2 - x1))
+						+ ((y2 - y1) * (y2 - y1)));
+
+				matrix[i][j] = distance;
+			}
+		}
+
+		return matrix;
+
+	}
+
+	/**
+	 * Calculates the answer to TSP.
+	 * @param adjacencyMatrix 2D adjacency matrix.
+	 * @return returns a solution to TSP in the form of an array of cities
+	 */
+	public static int[] tsp(double adjacencyMatrix[][]) {
+
+		int[] path = new int[adjacencyMatrix[0].length];
+
+		path[0] = 0;
+		int currentCity = 0;
+
+		int i = 1;
+		while (i < path.length) {
+			// find next city
+			int nextCity = findMin(adjacencyMatrix[currentCity], path);
+			// if the city is not -1 (meaning if there is a city to be visited
+			if (nextCity != -1) {
+				// add the city to the path
+				path[i] = nextCity;
+				// update currentCity and i
+				currentCity = nextCity;
+				i++;
+			}
+		}
+
+		return path;
+	}
+
+	/**
+	 * Finds the minimum neighbor of a node that is not currently in the path.
+	 * @param row a row of an adjacency matrix.
+	 * @param path the current path of the tps method.
+	 * @return returns the next city.
+	 */
+	private static int findMin(double[] row, int[] path) {
+
+		int nextCity = -1;
+		int i = 0;
+		double min = Integer.MAX_VALUE;
+
+		while (i < row.length) {
+			if (isCityInPath(path, i) == false && row[i] < min) {
+				min = row[i];
+				nextCity = i;
+			}
+			i++;
+		}
+		return nextCity;
+	}
+
+	/**
+	 * Checks wether a city is in the current path
+	 * @param path current path of cities.
+	 * @param city a city
+	 * @return true if city is in the current path, false if it is not.
+	 */
+	public static boolean isCityInPath(int[] path, int city) {
+		for (int i = 0; i < path.length; i++) {
+			if (path[i] == city) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	/**
+	 * Method to execute calculations
+	 * @return returns the TSP path as an integer array of cities.
+	 */
+	public List<Integer> call() {
+		double[][] adj = calcAdjacencyMatrix(cities);
+		int[] path = tsp(adj);
+	
+		List<Integer> pathList = new ArrayList<Integer>();		
+		for (int index = 0; index < path.length; index++) {
+			pathList.add(path[index]);
+		}
+
+		return pathList;
+	}
+}
---
+++ src/tasks/TaskMandelbrotSet.java
@@ -0,0 +1,99 @@
+package tasks;
+
+import api.Task;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
+
+/**
+ * This is an implementation of the Task interface solving the Mandelbrot Set
+ * Problem
+ * 
+ * @author Simen Aakhus, Anders Kristiansen, Eivind Kristoffersen and Samson Svendsen
+ *
+ */
+
+public class TaskMandelbrotSet extends JFrame implements Task<Integer[][]> {
+
+	private double cornerReal;
+	private double cornerIm;
+	private double length;
+	private int n;
+	private int iterationLim;
+
+	/**
+	 * Constructor that initializes the variables
+	 * 
+	 * @param cornerReal
+	 *            real part of the coordinate of the lower left corner in the
+	 *            complex plane
+	 * @param cornerIm
+	 *            imaginary part of the coordinate of the lower left corner in
+	 *            the complex plane
+	 * @param length
+	 *            represents the edge length of a square in the complex plane
+	 * @param n
+	 *            complex plane is divided into n x n pixels
+	 * @param iterationLim
+	 *            defining which regions that are part of the Mandelbrot set
+	 */
+	public TaskMandelbrotSet(double cornerReal, double cornerIm, double length,
+			int n, int iterationLim) {
+		this.cornerReal = cornerReal;
+		this.cornerIm = cornerIm;
+		this.length = length;
+		this.n = n;
+		this.iterationLim = iterationLim;
+	}
+
+	public double getCornerReal() {
+		return cornerReal;
+	}
+
+	public double getCornerIm() {
+		return cornerIm;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public int getN() {
+		return n;
+	}
+
+	public int getIterationLim() {
+		return iterationLim;
+	}
+
+	@Override
+	/**
+	 * this method determines what complex numbers are part of the Mandelbrot Set
+	 * @return returns the 2D Integer array containing the complex numbers in the Mandelbrot set
+	 */
+	public Integer[][] call() {
+		int n = getN();
+		Integer[][] counts = new Integer[n][n];
+		for (int row = 0; row < n; row++) {
+			for (int col = 0; col < n; col++) {
+				double x = 0;
+				double y = 0;
+				double c_re = (col - n / 2.0) * getLength() / n;
+				double c_im = (row - n / 2.0) * getLength() / n;
+				int iterationCount = 0;
+				while (x * x + y * y <= 4 && iterationCount < getIterationLim()) {
+					double x_kplus1 = x * x - y * y + c_re;
+					y = 2 * x * y + c_im;
+					x = x_kplus1;
+					iterationCount++;
+				}
+				counts[col][row] = iterationCount;
+			}
+		}
+		return counts;
+	}
+
+}
