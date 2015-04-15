a402a11c213b3e66015921c60f54675f4d2aeaab
Simen Aakhus <aakhus.simen@gmail.com>
2015-04-14 19:02:29 -0700

    ComputerImpl attemt

--- src/system/ComputerImpl.java
+++ src/system/ComputerImpl.java
@@ -1,11 +1,15 @@
 package system;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import api.Result;
 import api.Task;
 
-public class ComputerImpl extends UnicastRemoteObject implements Computer {
+public class ComputerImpl<T> extends UnicastRemoteObject implements Computer {
 
 	/**
 	 * 
@@ -17,17 +21,46 @@ public class ComputerImpl extends UnicastRemoteObject implements Computer {
 	}
 
 	@Override
-	public <T> T execute(Task<T> t) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+	public Result<T> execute(Task t) throws RemoteException {
+		long begin = System.currentTimeMillis();
+		T value = (T) t.call();
+		long end = System.currentTimeMillis();
+		long time = end - begin;
+		Result<T> r = new Result<T>(value, time);
+		return r;
 	}
 	
 	public static void main(String[] args){
+		String spaceHost = args[0];
+		
+		if (System.getSecurityManager() == null){
+			System.setSecurityManager(new SecurityManager());
+		}
+		
+		try{
+			Computer c = new ComputerImpl();
+			
+			ComputerSpaceInterface space = (ComputerSpaceInterface) Naming.lookup("//" + spaceHost +"/");
+			space.register(c);
+			
+			System.out.println("Computer ready");	
+		}
+		catch (RemoteException e) {
+			System.err.println("ComputerImpl exception : ");
+			e.printStackTrace();
+		}
+		catch (MalformedURLException e) {
+			System.err.println("ComputerImpl exception : ");
+			e.printStackTrace();
+		}
+		catch (NotBoundException e) {
+			System.err.println("ComputerImpl exception : ");
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			System.err.println("ComputerImpl exception : ");
+			e.printStackTrace();
+		}
 		
 	}
 	
-	
-	
-	
-
 }
